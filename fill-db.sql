6ae0f5d9b998ca168705c15e84b4d4ccc283d61a
Andrei Druzhaev <andrewdruzhaev@gmail.com>
2022-02-27 13:01:27 +0300

    Create sql scripts to create & fill users table

---
+++ fill-db.sql
@@ -0,0 +1,9 @@
+INSERT INTO users(login, password, age, isDeleted) VALUES 
+('Mark', 'qwe', 27, false),
+('Tony', '123', 20, false),
+('Mary', '123', 40, false),
+('Kate', '123', 20, false),
+('Andrew', '123', 20, false),
+('Zara', '123', 20, false),
+('Yura', '123', 20, false),
+('Polly', '123', 20, false);
